a437e2c78835ac46c76c3b99a825c7c20c382f94
Adarsh-shakya <adarsh.shakya_cs202gla.ac.in>
2022-01-13 11:50:06 +0530

    duplicate value

---
+++ src/Ckeck_Duplicate.java
@@ -0,0 +1,18 @@
+public class Ckeck_Duplicate {
+    public static  void main(String arg[]){
+        int arr[]={2,3,4,5,3,7,4};
+        for (int i = 0; i < arr.length ; i++) {
+            int count=0;
+            for (int j = 0; j <arr.length ; j++) {
+                if (arr[i]==arr[j])
+                {
+                    count++;
+                    if (count>1 && i!=j)
+                    {
+                        System.out.println("Index "+i+" and Index "+j+" have duplicate value");
+                    }
+                }
+            }
+        }
+    }
+}
