fb6c7e9c734e8d3cee7883b2a58d5e9cc0ae9199
Adarsh-shakya <adarsh.shakya_cs202gla.ac.in>
2022-01-21 21:21:23 +0530

    power Recur

---
+++ src/RecursionPower.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class RecursionPower {
+    public static int power(int n,int p){
+      if(p==0){
+          return  1;
+      }
+       int temp= power(n,p-1);
+        int ans=n*temp;
+        return ans;
+    }
+
+}
+class PowerMain{
+    public static void main(String[] args) {
+        {
+            Scanner sca=new Scanner(System.in);
+            int n;
+            int p;
+            System.out.println("Enter the number and power: ");
+            n=sca.nextInt();
+            p=sca.nextInt();
+            System.out.println(RecursionPower.power(2,3));
+
+        }
+    }
+}
