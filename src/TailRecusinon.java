0ece16ef7dc9d1ff57d8f9e9236e32f478507c5e
Adarsh-shakya <adarsh.shakya_cs202gla.ac.in>
2022-01-21 20:26:28 +0530

    Recursion1

---
+++ src/TailRecusinon.java
@@ -0,0 +1,15 @@
+public class TailRecusinon {
+    public static void fact(int n, int ans){
+        if(n==1)
+        {
+            System.out.print("Factorial= "+ans);
+            return;
+        }
+        fact(n-1,ans*n);
+    }
+}
+class tailMain{
+    public static void main(String []args){
+        TailRecusinon.fact(4,1);
+    }
+}
