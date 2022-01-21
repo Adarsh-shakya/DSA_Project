cb49be7d2dd06ff4589444805fbecb29730ea944
Adarsh-shakya <adarsh.shakya_cs202gla.ac.in>
2022-01-21 19:58:10 +0530

    Recursion

---
+++ src/Recursion.java
@@ -0,0 +1,15 @@
+public class Recursion {
+    public static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        int recuAnswer=fact(n-1);
+        int result=n*recuAnswer;
+        return result;
+    }
+}
+class RecuMain{
+    public static void main(String[] args) {
+        System.out.println("Facturial= "+ Recursion.fact(5));
+    }
+}
