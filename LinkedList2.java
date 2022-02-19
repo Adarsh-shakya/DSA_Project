f2c93bcb7984c6c27440a0d8d8c7c90bc74c1118
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2022-02-19 22:28:02 +0530

    Remove duplicate element

--- LinkedList2.java
+++ LinkedList2.java
@@ -1,13 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
-/**
- *
- * @author hp
- */
-public class LinkedList2 {
-    
+class Node
+{
+int data;
+Node next;
 }
+class LinkedList2
+{
+  Node Start;
+  LinkedList2()
+  {
+      Start=null;
+  }
+  public void CreateList()
+  {
+    Scanner sca=new Scanner(System.in);
+      System.out.println("Enter the list with 10 element: ");
+      for (int i = 0; i <10 ; i++) {
+          Node n=new Node();
+          n.data=sca.nextInt();
+          n.next=null;
+          if(Start==null)
+          {
+              Start=n;
+          }
+          else
+          {
+              Node current=Start;
+              while(current.next!=null)
+              {
+                  current=current.next;
+              }
+              current.next=n;
+          }
+      }
+
+  }
+  public void Unduplicate()
+  {
+      Node current=Start;
+      for ( current = Start; current.next!=null ; current=current.next)
+      {
+       if(current.data==current.next.data)
+       {
+           current.next=current.next.next;
+       }
+      }
+
+  }
+  public void Traverse()
+    {
+       Node current=Start;
+       for (current=Start;current!=null;current=current.next)
+           System.out.print(current.data+" ");
+    }
+
+    public static void main(String[] args) {
+        LinkedList2 obj=new LinkedList2();
+        obj.CreateList();
+        obj.Unduplicate();
+        obj.Traverse();
+    }
+}
+
